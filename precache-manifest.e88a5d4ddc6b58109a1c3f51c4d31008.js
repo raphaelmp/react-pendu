27f194d1bf6e03d6874be4f21c8f26912ea389c2
Raphaël M-P <rmarquispelletier@gmail.com>
2019-09-23 15:22:44 -0400

    Updates

---
+++ precache-manifest.e88a5d4ddc6b58109a1c3f51c4d31008.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa7915511cc15c64ef27e2bf461ab8a",
+    "url": "/react-pendu/index.html"
+  },
+  {
+    "revision": "9d432242f7f3f6c43778",
+    "url": "/react-pendu/static/css/main.ce7702f3.chunk.css"
+  },
+  {
+    "revision": "f3044473224028811d42",
+    "url": "/react-pendu/static/js/2.8b5038a6.chunk.js"
+  },
+  {
+    "revision": "9d432242f7f3f6c43778",
+    "url": "/react-pendu/static/js/main.cf0ca737.chunk.js"
+  },
+  {
+    "revision": "255f88f3c8cf01eff562",
+    "url": "/react-pendu/static/js/runtime~main.79249521.js"
+  }
+]);
